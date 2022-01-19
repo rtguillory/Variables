9afa017010c66988fc5c8ef3493189a3b8db2143
Rhonda Guillory <guilloryrt.ps@gmail.com>
2022-01-18 18:53:49 -0600

    initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Boba";
+int intNum = 5;
+char char1 = 'a';
+bool bool1 = false;
+double dbl = 1.5;
+decimal decimal1 = 2.1m;
+
+Console.WriteLine($"He is {intNum} years old and he has {dbl} dollars.");
+Console.WriteLine($"It is {bool1} that his name, {name}, starts with {char1}.");
